9826a919bd1bed98f78dd85af2b9d35ceb5805c0
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2024-08-01 09:44:38 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'game-node-mobile',
+  webDir: 'dist'
+};
+
+export default config;
