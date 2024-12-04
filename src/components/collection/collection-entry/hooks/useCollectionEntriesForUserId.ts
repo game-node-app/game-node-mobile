974276256a9a83f7b167a5e8434aee80a2c2bd83
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2024-12-03 22:05:25 -0300

    -

--- src/components/collection/collection-entry/hooks/useCollectionEntriesForUserId.ts
+++ src/components/collection/collection-entry/hooks/useCollectionEntriesForUserId.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { CollectionsEntriesService, CollectionsService, type FindCollectionEntriesOrderBy } from "@/wrapper/server";
+import { CollectionsEntriesService, type FindCollectionEntriesOrderBy } from "@/wrapper/server";
 
 export default function useCollectionEntriesForUserId(
     userId: string,
